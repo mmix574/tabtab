d2456af8d1d8c3554dd6c4072bb8ce10f3d74a02
taita2015 <mmix.xia@gmail.com>
2017-01-31 19:44:07 +0800

    initial commit

---
+++ js/newtab.js
@@ -0,0 +1,37 @@
+var engine_list = document.querySelectorAll(".btn-search");
+
+engine_list.forEach(function(engine) {
+    engine.addEventListener("click", function() {
+        var search_content = this.previousSibling.previousSibling.value;
+        var search_engine = this.nextSibling.nextSibling.value;
+        if (search_content) {
+            do_search(search_engine,search_content);
+        }
+    });
+});
+
+document.onkeydown = function(event) {
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    if (e && e.keyCode == 13) { // enter key pushdown
+        if (document.activeElement.tagName.toLowerCase() == "input") {
+            var search_engine=document.activeElement.nextSibling.nextSibling.nextSibling.nextSibling.value;
+            var search_content=document.activeElement.value;
+            do_search(search_engine,search_content);
+        }
+    }
+};
+
+var search_url = {
+    "baidu": "https://www.baidu.com/s?wd=",
+    "google": "https://www.google.com.hk/#&q=",
+    "bing": "http://cn.bing.com/search?q=",
+    "sougou": "https://www.sogou.com/web?query=",
+    "stackoverflow": "https://www.google.com.hk/#&q=site:stackoverflow.com ",
+    "zhihu": "https://www.google.com.hk/#&q=site:zhihu.com "
+}
+
+
+var do_search = function(engine, content) {
+    var newUrl = search_url[engine]+content;
+    window.location.href=newUrl;
+}
---
+++ record.md
@@ -0,0 +1,8 @@
+project started at:
+
+​	2017.01.31
+
+
+
+Icon url：
+http://www.iconarchive.com/show/mushroom-icons-by-jommans/Search-icon.html
